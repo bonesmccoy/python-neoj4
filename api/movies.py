4fea00a0af46357c1a3d5e00f076d6e636ba8ba8
Daniele Murroni <daniele.murroni@gmail.com>
2016-08-31 11:14:47 +0100

    add movie details

--- api/movies.py
+++ api/movies.py
@@ -1,4 +1,5 @@
 from graph.movie_graph import MovieGraph
+from py2neo import Node, Relationship
 
 
 def movies(actor=None):
@@ -7,3 +8,25 @@ def movies(actor=None):
 
     return [dict(movie['m']) for movie in results]
 
+
+def movie_details(title):
+    graph = MovieGraph()
+    result = graph.details(title)
+
+    actors = []
+    producers = []
+    directors = []
+    movie = None
+    for record in result:
+        if not movie:
+            movie = record['m']
+        relationship = record['r']
+        person = record['p']
+        if relationship.type() == 'ACTED_IN':
+            actors.append(dict(person))
+        if relationship.type() == 'PRODUCED':
+            producers.append(dict(person))
+        if relationship.type() == 'DIRECTED':
+            directors.append(dict(person))
+
+    return dict({"details": movie, "actors": actors, "producers": producers, "directors": directors})
--- graph/movie_graph.py
+++ graph/movie_graph.py
@@ -12,3 +12,10 @@ class MovieGraph(BaseGraph):
         query = "MATCH (m:Movie)<-[:ACTED_IN]-(p:Person) " + where + " RETURN m LIMIT {limit}"
 
         return self.graph.run(query, {"limit": 100})
+
+    def details(self, title):
+        query = "MATCH (m:Movie)<-[r]-(p:Person) \
+                WHERE m.title = '" + title + "' \
+                RETURN m,r,p"
+
+        return self.graph.run(query)
